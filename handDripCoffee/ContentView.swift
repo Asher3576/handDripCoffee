b3034548a6afc25574ba08594c4192e6c9c5b32d
asher <dud3576@naver.com>
2022-04-28 09:32:35 +0900

    Initial Commit

---
+++ handDripCoffee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  handDripCoffee
+//
+//  Created by 임영후 on 2022/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ handDripCoffee/handDripCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  handDripCoffeeApp.swift
+//  handDripCoffee
+//
+//  Created by 임영후 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct handDripCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
